0fa6009e2169f00514866f673670c9b7680bd708
ddonfack <ddonfack@dative-gpi.com>
2025-04-09 08:20:27 +0200

    update url service account

--- src/core/foundation-core-services/config/urls/serviceAccountOrganisationAuthTokens.ts
+++ src/core/foundation-core-services/config/urls/serviceAccountOrganisationAuthTokens.ts
@@ -1,4 +1,4 @@
 import { CORE_URL } from "./base";
 
-export const SERVICE_ACCOUNT_ORGANISATION_AUTH_TOKENS_URL = () => `${CORE_URL()}/pats`;
+export const SERVICE_ACCOUNT_ORGANISATION_AUTH_TOKENS_URL = () => `${CORE_URL()}/service-account-organisation-auth-tokens`;
 export const SERVICE_ACCOUNT_ORGANISATION_AUTH_TOKEN_URL = (authTokenId: string) => `${SERVICE_ACCOUNT_ORGANISATION_AUTH_TOKENS_URL()}/${encodeURIComponent(authTokenId)}`;
