210dc607f49ce34575f53287418733b605fd772b
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2025-04-16 20:21:46 +0000

    deploy: 546c34dcd1c901702666278bf19094ab4156ea5a

---
+++ assets/DashboardOrganisationType.stories-BvMRCYiX.js
@@ -0,0 +1,40 @@
+import{d as f,q as b,w as T,z as n,E as h,m as O,G as s,H as p}from"./vue.esm-bundler-PTWG9x2X.js";import{F as v}from"./FSDashboardOrganisationTypeTileUI-BzCNR2Bp.js";import{F as I}from"./FSLoadTile-t9RE8sse.js";import{a as V}from"./useDashboardOrganisationTypes-BaFYBVLq.js";import{_ as F}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSSimpleTileUI-B8kllFdB.js";import"./FSIconCard-CVsCm8Sv.js";import"./FSCard-CRoBY3gL.js";import"./FSCol-qfATdd_b.js";import"./css-B90wD3BD.js";import"./useBreakpoints-W7XGW48y.js";import"./FSRow-DPcQMiM9.js";import"./useColors-DSUu_QQ4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-D6MQh39g.js";import"./FSIcon-DQMOs0-E.js";import"./VIcon-DIeegzqR.js";import"./color-CCpgnDIk.js";import"./FSImage-D6wIZvVb.js";import"./FSImageUI-Cd9yfhhj.js";import"./FSLoader-DnFcYh1x.js";import"./dimensions-v18atrWI.js";import"./elevation-CrSwNJwg.js";import"./locale-Cz_4DTJh.js";import"./VImg-CBGPWIu-.js";import"./rounded-BbPXzFqc.js";import"./transition-QV8dDJB9.js";import"./index-BNomsh5u.js";import"./useImages-B58XKdkx.js";import"./base-CmdGny12.js";import"./composableFactory-BVozujU4.js";import"./serviceFactory-B3o6Bvwt.js";import"./lodash-BrWYvtk2.js";import"./uuid-DTaye2KM.js";import"./useAppAuthToken-3n1rBHGl.js";import"./FSSpan-yJEd2pn-.js";import"./useSlots-BG_ZhEqK.js";import"./FSTile-oBYIwUj0.js";import"./FSClickable-KpP1cmTZ.js";import"./FSRouterLink-C06pLVSG.js";import"./vue-router-_5OpMgKw.js";import"./VProgressCircular-eoxb555W.js";import"./resizeObserver-C17Kgm90.js";import"./FSCheckbox-D0eLEsDk.js";import"./useRules-D0leB7Wy.js";import"./VSelectionControl-Fvr0iuTu.js";import"./density-CznkHGGy.js";import"./proxiedModel-CvHSoKPb.js";import"./index-Dqqa47M6.js";import"./VLabel-BNJ33aDR.js";import"./VInput-C2FcZlE6.js";import"./index-b-ADC_Il.js";import"./dashboardTranslation-BY3Wh2RO.js";import"./useAppLanguageCode-tS_QL_TH.js";import"./base-BJ27TNVe.js";import"./useAppOrganisationId-CurthI5S.js";const u=f({name:"FSDashboardOrganisationTypeTile",components:{FSDashboardOrganisationTypeTileUI:v,FSLoadTile:I},props:{dashboardOrganisationTypeId:{type:String,required:!0},modelValue:{type:Boolean,required:!1,default:!1},selectable:{type:Boolean,required:!1,default:!0}},setup(e){const{get:a,getting:t,entity:i}=V();return b(()=>{a(e.dashboardOrganisationTypeId)}),T(()=>e.dashboardOrganisationTypeId,()=>{a(e.dashboardOrganisationTypeId)}),{getting:t,entity:i}}});function S(e,a,t,i,k,D){const c=s("FSLoadTile"),y=s("FSDashboardOrganisationTypeTileUI");return e.getting?(p(),n(c,{key:0,selectable:e.$props.selectable,modelValue:e.$props.modelValue,"onUpdate:modelValue":a[0]||(a[0]=r=>e.$emit("update:modelValue",r))},null,8,["selectable","modelValue"])):e.entity?(p(),n(y,O({key:1,code:e.entity.code,icon:e.entity.icon,label:e.entity.label,imageId:e.entity.imageId,selectable:e.$props.selectable,bottomColor:e.entity.colors,modelValue:e.$props.modelValue,"onUpdate:modelValue":a[1]||(a[1]=r=>e.$emit("update:modelValue",r))},e.$attrs),null,16,["code","icon","label","imageId","selectable","bottomColor","modelValue"])):h("",!0)}const g=F(u,[["render",S]]);u.__docgenInfo={displayName:"FSDashboardOrganisationTypeTile",exportName:"default",description:"",tags:{},props:[{name:"dashboardOrganisationTypeId",type:{name:"string"},required:!0},{name:"modelValue",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"false"}},{name:"selectable",type:{name:"boolean"},required:!1,defaultValue:{func:!1,value:"true"}}],events:[{name:"update:modelValue"}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/core/foundation-core-components/components/tiles/FSDashboardOrganisationTypeTile.vue"]};const Ce={title:"Foundation/Core/Tiles/DashboardOrganisationType",component:g,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},o={args:{args:{values:["1","2","3"],selected:[!1,!1,!1]}},render:(e,{argTypes:a})=>({components:{FSDashboardOrganisationTypeTile:g},props:Object.keys(a),setup(){return{...e}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardOrganisationTypeTile
+        v-for="(dashboardOrganisationTypeId, index) in args.values"
+        :key="index"
+        :dashboardOrganisationTypeId="dashboardOrganisationTypeId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>`})};var l,d,m;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: ["1", "2", "3"],
+      selected: [false, false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSDashboardOrganisationTypeTile
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSDashboardOrganisationTypeTile
+        v-for="(dashboardOrganisationTypeId, index) in args.values"
+        :key="index"
+        :dashboardOrganisationTypeId="dashboardOrganisationTypeId"
+        @auxclick="args.values.pop()"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(m=(d=o.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const xe=["Variations"];export{o as Variations,xe as __namedExportsOrder,Ce as default};
---
+++ assets/Group.stories-BFGYaRdd.js
@@ -0,0 +1,141 @@
+import{F as o}from"./FSGroupTileUI-DSqLBd0G.js";import"./vue.esm-bundler-PTWG9x2X.js";import"./FSIconCard-CVsCm8Sv.js";import"./FSCard-CRoBY3gL.js";import"./FSCol-qfATdd_b.js";import"./css-B90wD3BD.js";import"./useBreakpoints-W7XGW48y.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-DPcQMiM9.js";import"./useColors-DSUu_QQ4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-D6MQh39g.js";import"./FSIcon-DQMOs0-E.js";import"./VIcon-DIeegzqR.js";import"./color-CCpgnDIk.js";import"./FSImage-D6wIZvVb.js";import"./FSImageUI-Cd9yfhhj.js";import"./FSLoader-DnFcYh1x.js";import"./dimensions-v18atrWI.js";import"./elevation-CrSwNJwg.js";import"./locale-Cz_4DTJh.js";import"./VImg-CBGPWIu-.js";import"./rounded-BbPXzFqc.js";import"./transition-QV8dDJB9.js";import"./index-BNomsh5u.js";import"./useImages-B58XKdkx.js";import"./base-CmdGny12.js";import"./composableFactory-BVozujU4.js";import"./serviceFactory-B3o6Bvwt.js";import"./lodash-BrWYvtk2.js";import"./uuid-DTaye2KM.js";import"./useAppAuthToken-3n1rBHGl.js";import"./FSColor-D8vECu2f.js";import"./FSSpan-yJEd2pn-.js";import"./useSlots-BG_ZhEqK.js";import"./FSTile-oBYIwUj0.js";import"./FSClickable-KpP1cmTZ.js";import"./FSRouterLink-C06pLVSG.js";import"./vue-router-_5OpMgKw.js";import"./VProgressCircular-eoxb555W.js";import"./resizeObserver-C17Kgm90.js";import"./FSCheckbox-D0eLEsDk.js";import"./useRules-D0leB7Wy.js";import"./VSelectionControl-Fvr0iuTu.js";import"./density-CznkHGGy.js";import"./proxiedModel-CvHSoKPb.js";import"./index-Dqqa47M6.js";import"./VLabel-BNJ33aDR.js";import"./VInput-C2FcZlE6.js";import"./index-b-ADC_Il.js";const pr={title:"Foundation/Shared/Tiles/Group",component:o,tags:["autodocs"],argTypes:{onClick:{action:"clicked"}}},e={args:{args:{values:[{id:"1",label:"Can't hold us",code:"Macklemore & Ryan Lewis",imageId:"1",recursiveGroupsIds:[],recursiveDeviceOrganisationsIds:[]},{id:"2",label:"Paint the town red",code:"Doja Cat",imageId:"1",recursiveGroupsIds:Array.from(Array(4).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(8).keys()).map(r=>r.toString())},{id:"3",label:"Who's ready for tomorrow",code:"Rat Boy",imageId:"1",recursiveGroupsIds:Array.from(Array(4).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(16).keys()).map(r=>r.toString())},{id:"4",label:"Dynasties & Dystopia",code:"Denzel Curry featuring Gizzle and Bren Joy",imageId:"1",recursiveGroupsIds:Array.from(Array(100).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(100).keys()).map(r=>r.toString())}],selected:[!1,!1]}},render:(r,{argTypes:s})=>({components:{FSGroupTileUI:o},props:Object.keys(s),setup(){return{...r}},template:`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSGroupTileUI
+        v-for="(group, index) in args.values"
+        :key="index"
+        :imageId="group.imageId"
+        :label="group.label"
+        :code="group.code"
+        :recursiveGroupsIds="group.recursiveGroupsIds"
+        :recursiveDeviceOrganisationsIds="group.recursiveDeviceOrganisationsIds"
+        v-model="args.selected[index]"
+      />
+    </div>`})},i={args:{args:{values:[{id:"1",label:"Can't hold us",code:"Macklemore & Ryan Lewis",recursiveGroupsIds:[],recursiveDeviceOrganisationsIds:[]},{id:"2",label:"Paint the town red",code:"Doja Cat",recursiveGroupsIds:Array.from(Array(4).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(8).keys()).map(r=>r.toString())},{id:"3",label:"Who's ready for tomorrow",code:"Rat Boy",recursiveGroupsIds:Array.from(Array(4).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(16).keys()).map(r=>r.toString())},{id:"4",label:"Dynasties & Dystopia",code:"Denzel Curry featuring Gizzle and Bren Joy",recursiveGroupsIds:Array.from(Array(100).keys()).map(r=>r.toString()),recursiveDeviceOrganisationsIds:Array.from(Array(100).keys()).map(r=>r.toString())}],selected:[!1,!1]}},render:(r,{argTypes:s})=>({components:{FSGroupTileUI:o},props:Object.keys(s),setup(){return{...r}},template:`
+  <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+    <FSGroupTileUI
+      v-for="(group, index) in args.values"
+      :key="index"
+      :label="group.label"
+      :code="group.code"
+      :recursiveGroupsIds="group.recursiveGroupsIds"
+      :recursiveDeviceOrganisationsIds="group.recursiveDeviceOrganisationsIds"
+      v-model="args.selected[index]"
+    />
+  </div>`})};var a,n,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: [{
+        id: "1",
+        label: "Can't hold us",
+        code: "Macklemore & Ryan Lewis",
+        imageId: "1",
+        recursiveGroupsIds: [],
+        recursiveDeviceOrganisationsIds: []
+      }, {
+        id: "2",
+        label: "Paint the town red",
+        code: "Doja Cat",
+        imageId: "1",
+        recursiveGroupsIds: Array.from(Array(4).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(8).keys()).map(i => i.toString())
+      }, {
+        id: "3",
+        label: "Who's ready for tomorrow",
+        code: "Rat Boy",
+        imageId: "1",
+        recursiveGroupsIds: Array.from(Array(4).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(16).keys()).map(i => i.toString())
+      }, {
+        id: "4",
+        label: "Dynasties & Dystopia",
+        code: "Denzel Curry featuring Gizzle and Bren Joy",
+        imageId: "1",
+        recursiveGroupsIds: Array.from(Array(100).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(100).keys()).map(i => i.toString())
+      }],
+      selected: [false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSGroupTileUI
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+      <FSGroupTileUI
+        v-for="(group, index) in args.values"
+        :key="index"
+        :imageId="group.imageId"
+        :label="group.label"
+        :code="group.code"
+        :recursiveGroupsIds="group.recursiveGroupsIds"
+        :recursiveDeviceOrganisationsIds="group.recursiveDeviceOrganisationsIds"
+        v-model="args.selected[index]"
+      />
+    </div>\`
+  })
+}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var p,d,m;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    args: {
+      values: [{
+        id: "1",
+        label: "Can't hold us",
+        code: "Macklemore & Ryan Lewis",
+        recursiveGroupsIds: [],
+        recursiveDeviceOrganisationsIds: []
+      }, {
+        id: "2",
+        label: "Paint the town red",
+        code: "Doja Cat",
+        recursiveGroupsIds: Array.from(Array(4).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(8).keys()).map(i => i.toString())
+      }, {
+        id: "3",
+        label: "Who's ready for tomorrow",
+        code: "Rat Boy",
+        recursiveGroupsIds: Array.from(Array(4).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(16).keys()).map(i => i.toString())
+      }, {
+        id: "4",
+        label: "Dynasties & Dystopia",
+        code: "Denzel Curry featuring Gizzle and Bren Joy",
+        recursiveGroupsIds: Array.from(Array(100).keys()).map(i => i.toString()),
+        recursiveDeviceOrganisationsIds: Array.from(Array(100).keys()).map(i => i.toString())
+      }],
+      selected: [false, false]
+    }
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSGroupTileUI
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        ...args
+      };
+    },
+    template: \`
+  <div style="display: flex; gap: 10px; flex-wrap: wrap; width: 100vw;">
+    <FSGroupTileUI
+      v-for="(group, index) in args.values"
+      :key="index"
+      :label="group.label"
+      :code="group.code"
+      :recursiveGroupsIds="group.recursiveGroupsIds"
+      :recursiveDeviceOrganisationsIds="group.recursiveDeviceOrganisationsIds"
+      v-model="args.selected[index]"
+    />
+  </div>\`
+  })
+}`,...(m=(d=i.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const dr=["Variations","Imageless"];export{i as Imageless,e as Variations,dr as __namedExportsOrder,pr as default};
