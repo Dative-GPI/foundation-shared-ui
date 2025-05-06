a4cb5dbe0b195be866482be144eee946a7467702
peyronth <peyron.th@gmail.com>
2025-05-06 16:59:13 +0200

    Add description and label default for role Organisation details

--- src/core/foundation-core-domain/models/roleOrganisations/roleOrganisationDetails.ts
+++ src/core/foundation-core-domain/models/roleOrganisations/roleOrganisationDetails.ts
@@ -3,19 +3,25 @@ import { RoleOrganisationInfos, type RoleOrganisationInfosDTO } from "./roleOrga
 import { type RoleType } from "@dative-gpi/foundation-shared-domain/enums";
 
 export class RoleOrganisationDetails extends RoleOrganisationInfos {
+  labelDefault: string;
   description: string;
+  descriptionDefault: string;
   translations: RoleTranslation[];
 
   constructor(params: RoleOrganisationDetailsDTO) {
     super(params);
 
+    this.labelDefault = params.labelDefault;
     this.description = params.description;
+    this.descriptionDefault = params.descriptionDefault;
     this.translations = params.translations.map(t => new RoleTranslation(t));
   }
 }
 
 export interface RoleOrganisationDetailsDTO extends RoleOrganisationInfosDTO {
+  labelDefault: string;
   description: string;
+  descriptionDefault: string;
   translations: RoleTranslationDTO[];
 }
 
