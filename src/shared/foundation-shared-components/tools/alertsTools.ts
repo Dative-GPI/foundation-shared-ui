9147a0a830286b11df39df868f88e497b4ffc958
gballigand <balligandgm@gmail.com>
2025-05-06 14:56:56 +0200

    move foundaiton tool to shared-ui

--- src/shared/foundation-shared-components/tools/alertsTools.ts
+++ src/shared/foundation-shared-components/tools/alertsTools.ts
@@ -1,6 +1,6 @@
 import { useTranslations as useTranslationsProvider } from "@dative-gpi/bones-ui/composables";
 import { AlertStatus, Criticity } from "@dative-gpi/foundation-shared-domain/enums";
-import { ColorEnum } from "@dative-gpi/foundation-shared-components/models"
+import { ColorEnum } from "../models"
 import { getTimeBestString } from "../utils";
 
 const { $tr } = useTranslationsProvider();
@@ -20,13 +20,40 @@ export const AlertTools = {
   },
   statusLabel(value: AlertStatus): string {
     switch (value) {
-      case AlertStatus.Pending: return $tr("ui.alert-status.pending", "Pending");
-      case AlertStatus.Untriggered: return $tr("ui.alert-status.untriggered", "Untriggered");
-      case AlertStatus.Unresolved: return $tr("ui.alert-status.unresolved", "Unresolved");
-      case AlertStatus.Resolved: return $tr("ui.alert-status.resolved", "Resolved");
-      case AlertStatus.Expired: return $tr("ui.alert-status.expired", "Expired");
-      case AlertStatus.Triggered: return $tr("ui.alert-status.triggered", "Triggered");
-      case AlertStatus.Abandoned: return $tr("ui.alert-status.abandoned", "Abandoned");
+      case AlertStatus.Pending:
+        return $tr('ui.alert-status.pending','Pending');
+      case AlertStatus.Untriggered:
+        return $tr('ui.alert-status.untriggered','Untriggered');
+      case AlertStatus.Unresolved:
+        return $tr('ui.alert-status.unresolved','Unresolved');
+      case AlertStatus.Resolved:
+        return $tr('ui.alert-status.resolved','Resolved');
+      case AlertStatus.Expired:
+        return $tr('ui.alert-status.expired','Expired');
+      case AlertStatus.Triggered:
+        return $tr('ui.alert-status.triggered','Triggered');
+      case AlertStatus.Abandoned:
+        return $tr('ui.alert-status.abandoned','Abandoned');
+      default: return "";
+    }
+  },
+  statusAlternativeColor(value: AlertStatus): string {
+    switch (value) {
+      case AlertStatus.Pending:
+      case AlertStatus.Unresolved:
+      case AlertStatus.Triggered: return "#da2d2d";
+      default: return "#1e56e5";
+    }
+  },
+  statusAlternativeIcon(value: AlertStatus): string {
+    switch (value) {
+      case AlertStatus.Pending: return "timer";
+      case AlertStatus.Untriggered: return "timer_off";
+      case AlertStatus.Unresolved: return "error_outline";
+      case AlertStatus.Resolved: return "check_circle_outline";
+      case AlertStatus.Expired: return "schedule";
+      case AlertStatus.Triggered: return "error_outline";
+      case AlertStatus.Abandoned: return "block"
       default: return "";
     }
   },
@@ -47,23 +74,23 @@ export const AlertTools = {
   },
   criticityLabel(value: Criticity): string {
     switch (value) {
-      case Criticity.Warning: return $tr("ui.common.warning", "Warning");
-      case Criticity.Error: return $tr("ui.common.error", "Error");
-      case Criticity.Information: return $tr("ui.common.information", "Information")
-      default: return $tr("ui.common.none", "None");
+      case Criticity.Warning: return $tr('ui.common.warning','Warning');
+      case Criticity.Error: return $tr('ui.common.error','Error');
+      default: return $tr('ui.common.information','Information');
     }
   }
 }
 
-export const prettyDuration = (n: number) => {
+export const prettyDuration = (n: number | undefined) => {
+  if (n == null) { return "00m 00s"; }
   const day = Math.floor(n / 24 / 60 / 60);
   const hours = Math.floor((n - day * 24 * 60 * 60) / 3600);
   const minutes = Math.floor((n - day * 24 * 60 * 60 - hours * 60 * 60) / 60);
   const seconds = Math.floor(n - day * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60);
 
   return day > 0 ?
-    `${getTimeBestString(day * 24 * 60 * 60)} ${getTimeBestString(hours * 60 * 60)} ${getTimeBestString(minutes * 60)} ${getTimeBestString(seconds)}`
+    `${getTimeBestString(day * 24 * 60 * 60)} ${hours > 0 ? getTimeBestString(hours * 60 * 60) : ''} ${minutes > 0 ? getTimeBestString(minutes * 60) : ''} ${getTimeBestString(seconds)}`
     : hours > 0
-      ? `${getTimeBestString(hours * 60 * 60)} ${getTimeBestString(minutes * 60)} ${getTimeBestString(seconds)}`
-      : `${getTimeBestString(minutes * 60)} ${getTimeBestString(seconds)}`;
+      ? `${getTimeBestString(hours * 60 * 60)} ${minutes > 0 ? getTimeBestString(minutes * 60) : ''} ${getTimeBestString(seconds)}`
+      : `${minutes > 0 ? getTimeBestString(minutes * 60) : ''} ${getTimeBestString(seconds)}`;
 };
