a05ffee864f61720385f1bc7975d9cbe68869547
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2025-05-21 15:12:50 +0000

    deploy: ff9e970b73413e8c330cd390023a52193a539eac

---
+++ assets/Status.stories-B0FMkTG_.js
@@ -0,0 +1,34 @@
+import{F as e}from"./FSStatus-dw-FeLUY.js";import"./vue.esm-bundler-CM4TIH-Q.js";import"./FSCard-CYsPoqay.js";import"./FSCol-Be93XvPD.js";import"./css-D38lF7BW.js";import"./useBreakpoints-DsICqL2A.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSRow-C47sfGk6.js";import"./useColors-CjA2D2xA.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DfSX31J9.js";import"./theme-d3-Spt7o.js";import"./FSChip-FDBATtNs.js";import"./FSIcon-CIF0zaTI.js";import"./VIcon-GfKFLOEw.js";import"./color-Bo6l13w9.js";import"./FSSpan-BXQyelfo.js";import"./useSlots-c6yg-hhg.js";import"./FSRouterLink-CxLK3Rak.js";import"./vue-router-io_1IWnU.js";import"./FSText-C032WjuG.js";import"./useDateFormat-hcE1Scoo.js";import"./useAppTimeZone-CP1Ic-gf.js";import"./datesTools-DpylUQoJ.js";import"./startOfWeek-uXTpkxA4.js";import"./startOfDay-C4pDH4rb.js";import"./useAppLanguageCode-CN6bsmo6.js";import"./useTranslations-CLqRehXT.js";import"./FSColor-BRiM7uWQ.js";import"./FSMenu-Da0q64tf.js";import"./VMenu-sKuqJ8jm.js";import"./VOverlay-DtEUynQC.js";import"./forwardRefs-DWGaNmQL.js";import"./proxiedModel-DKx29MO-.js";import"./anchor-DCFmATqq.js";import"./dimensions-DW-k5kP_.js";import"./display-CUGteI3_.js";import"./scopeId-COl1Iexy.js";import"./locale-BqFH1fBg.js";import"./router-BXF-7Xxx.js";import"./transition-DMoMR0kB.js";import"./VDefaultsProvider-DYp4Zk7-.js";const Y={title:"Foundation/Shared/Status",component:e,tags:["autodocs"],argTypes:{}},m={id:"testId",label:"testLabel",index:1,showDefault:!1},i={label:"testLabelValue",value:"70",unit:"°C",icon:"mdi-thermometer",color:"#E01515",sourceTimestamp:1726547886e3,enqueuedTimestamp:1726547952e3,processedTimestamp:1726547958156},t={args:{modelStatus:m,statusGroup:i,size:"32",padding:"8px"},render:(p,{argTypes:a})=>({components:{FSStatus:e},props:Object.keys(a),setup(){return{args:p}},template:`
+      <FSStatus 
+        :modelStatus="args.modelStatus" 
+        :statusGroup="args.statusGroup" 
+        :size="args.size"
+        :padding="args.padding"
+      />`})};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    modelStatus: modelStatus,
+    statusGroup: statusGroup,
+    size: '32',
+    padding: '8px'
+  },
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      FSStatus
+    },
+    props: Object.keys(argTypes),
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSStatus 
+        :modelStatus="args.modelStatus" 
+        :statusGroup="args.statusGroup" 
+        :size="args.size"
+        :padding="args.padding"
+      />\`
+  })
+}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const Z=["Default"];export{t as Default,Z as __namedExportsOrder,Y as default};
