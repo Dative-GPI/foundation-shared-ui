bf101df7137ba7d3988767e9a6520295e4e4fbd2
SchroterQuentin <SchroterQuentin@users.noreply.github.com>
2025-06-02 10:09:35 +0000

    deploy: 0b95427ef66085007276fb4659a911d231726be1

--- assets/EntityView.stories-Bjt95yer.js
+++ assets/EntityView.stories-Bjt95yer.js
@@ -1,4 +1,15 @@
-import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}from"./vue.esm-bundler-DtArtqdn.js";import{F as T}from"./FSBaseView-BZ_cnyPr.js";import{_ as P}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as f,a as x}from"./FSButtonRemove-DNAzJ_lY.js";import{F as B}from"./FSLoader-C5CmIvK9.js";import{F as $}from"./FSImage-DyJ7YE9e.js";import{F as I}from"./FSCol-D6yYDi9X.js";import{C as s}from"./useColors-P6sW9vWR.js";import{V as k}from"./VDivider-CsMvCpx9.js";import"./FSRow-C-mN1bTt.js";import"./css-Byt5yd9p.js";import"./useBreakpoints-DjafNA1e.js";import"./FSText-CyTnv_LZ.js";import"./useSlots-DbyfVVj5.js";import"./FSBreadcrumbs-CQD77Qbn.js";import"./FSSpan-Btkwe66a.js";import"./FSIcon-DAU03b1P.js";import"./VIcon-_X0ionFS.js";import"./color-DKVd81aj.js";import"./theme-BTycGIF1.js";import"./router-DPM6bvc5.js";import"./density-96HGjcby.js";import"./rounded-B9giz-GX.js";import"./VDefaultsProvider-ChCmi1O1.js";import"./FSSlideGroup-DaeYUKJp.js";import"./uuid-DTaye2KM.js";import"./FSButtonNextIcon-Bvnrx82W.js";import"./FSButton-q8xhuZjd.js";import"./FSRouterLink-BnjgQQHT.js";import"./vue-router-D25_MSGh.js";import"./FSClickable-Lp9mb8CD.js";import"./FSCard-oEcmHswj.js";import"./VProgressCircular-vkSDrwPD.js";import"./resizeObserver-Bx252tqU.js";import"./VSlideGroupItem-XSA54cuy.js";import"./group-B-yFvi8t.js";import"./proxiedModel-DNPVb8sx.js";import"./index-YdMT2sR1.js";import"./display-H53nrFTM.js";import"./locale-EnvrzLNX.js";import"./FSFadeOut-okmKwNuP.js";import"./FSIconCard-B3mmomhn.js";import"./dimensions-Dt4h5jdR.js";import"./elevation-DuPJjm1T.js";import"./FSImageUI-CJROiAZk.js";import"./VImg-DB10nKT8.js";import"./transition-POm6u0ds.js";import"./index-ShtRROw2.js";import"./useImages-Ob47Tzpe.js";import"./base-CmdGny12.js";import"./composableFactory-D6jnN1uy.js";import"./serviceFactory-B3o6Bvwt.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BrWYvtk2.js";import"./useAppAuthToken-CqNI4hgs.js";import"./index-DfSX31J9.js";import"./color-CPMAIvbI.js";import"./theme-D430tkul.js";const E=h({name:"FSEntityView",components:{FSBaseView:T}});function q(e,n,N,W,G,H){const V=M("FSBaseView");return _(),D(V,R({variant:"entity"},e.$attrs),L({_:2},[A(e.$slots,(J,m)=>({name:m,fn:j(w=>[z(e.$slots,m,O(Z(w)))])}))]),1040)}const t=P(E,[["render",q]]);E.__docgenInfo={displayName:"FSEntityView",exportName:"default",description:"",tags:{},slots:[{name:"name",scoped:!0,bindings:[{name:"name",title:"binding"}]}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/views/FSEntityView.vue"]};const Ye={title:"Foundation/Shared/views/EntityView",component:t,tags:["autodocs"],argTypes:{}},o={args:{title:"MIZAR CDC",subtitle:"93000000000000128751",description:"Description d'un four Mizar sous Lorem Ipsum en maquettes.",imageCover:!0,icon:"mdi-chemical-weapon",iconBackgroundColors:["#FF0000","#0000FF"],iconBackgroundVariant:"gradient",iconBorder:!1,iconColor:s.Dark,breadcrumbs:[{title:"Lorem",to:"/"},{title:"MIZAR CDC",disabled:!0}]},render:(e,{argTypes:n})=>({components:{FSEntityView:t,FSButtonEdit:x,FSButtonRemove:f,FSLoader:B,FSCol:I},props:Object.keys(n),setup(){return{args:e}},template:`
+import{b as R}from"./properties-Qw-O9fbT.js";import{F as I}from"./FSCol-D6yYDi9X.js";import{F as M}from"./FSImage-DyJ7YE9e.js";import{F as x}from"./FSLoader-C5CmIvK9.js";import{F as w,a as Z,b as _,c as z,d as P}from"./FSBaseView-C3JSk5LB.js";import{d as $,z as j,B as q,S as N,m as O,G as T,H as W,C as G,N as H,O as J,P as K}from"./vue.esm-bundler-DtArtqdn.js";import{_ as Q}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{F as E,a as k}from"./FSButtonRemove-DNAzJ_lY.js";import{C as s}from"./useColors-P6sW9vWR.js";import{V as h}from"./VDivider-CsMvCpx9.js";import"./css-Byt5yd9p.js";import"./useBreakpoints-DjafNA1e.js";import"./FSImageUI-CJROiAZk.js";import"./VImg-DB10nKT8.js";import"./color-DKVd81aj.js";import"./theme-BTycGIF1.js";import"./dimensions-Dt4h5jdR.js";import"./rounded-B9giz-GX.js";import"./transition-POm6u0ds.js";import"./index-ShtRROw2.js";import"./useImages-Ob47Tzpe.js";import"./base-CmdGny12.js";import"./composableFactory-D6jnN1uy.js";import"./serviceFactory-B3o6Bvwt.js";import"./_commonjsHelpers-BosuxZz1.js";import"./lodash-BrWYvtk2.js";import"./uuid-DTaye2KM.js";import"./useAppAuthToken-CqNI4hgs.js";import"./elevation-DuPJjm1T.js";import"./locale-EnvrzLNX.js";import"./FSRow-C-mN1bTt.js";import"./FSText-CyTnv_LZ.js";import"./useSlots-DbyfVVj5.js";import"./FSBreadcrumbs-CQD77Qbn.js";import"./FSSpan-Btkwe66a.js";import"./FSIcon-DAU03b1P.js";import"./VIcon-_X0ionFS.js";import"./router-DPM6bvc5.js";import"./density-96HGjcby.js";import"./VDefaultsProvider-ChCmi1O1.js";import"./FSSlideGroup-DaeYUKJp.js";import"./FSButtonNextIcon-Bvnrx82W.js";import"./FSButton-q8xhuZjd.js";import"./FSRouterLink-BnjgQQHT.js";import"./vue-router-D25_MSGh.js";import"./FSClickable-Lp9mb8CD.js";import"./FSCard-oEcmHswj.js";import"./VProgressCircular-vkSDrwPD.js";import"./resizeObserver-Bx252tqU.js";import"./VSlideGroupItem-XSA54cuy.js";import"./group-B-yFvi8t.js";import"./proxiedModel-DNPVb8sx.js";import"./index-YdMT2sR1.js";import"./display-H53nrFTM.js";import"./FSFadeOut-okmKwNuP.js";import"./FSIconCard-B3mmomhn.js";import"./index-DfSX31J9.js";import"./color-CPMAIvbI.js";import"./theme-D430tkul.js";const D=$({name:"FSEntityView",components:{FSBaseView:w}});function U(e,X,Y,ee,te,ne){const A=T("FSBaseView");return W(),j(A,O({variant:"entity"},e.$attrs),q({_:2},[N(e.$slots,(oe,m)=>({name:m,fn:G(L=>[H(e.$slots,m,J(K(L)))])}))]),1040)}const t=Q(D,[["render",U]]);D.__docgenInfo={displayName:"FSEntityView",exportName:"default",description:"",tags:{},slots:[{name:"name",scoped:!0,bindings:[{name:"name",title:"binding"}]}],sourceFiles:["/home/runner/work/foundation-shared-ui/foundation-shared-ui/src/shared/foundation-shared-components/components/views/FSEntityView.vue"]};const lt={title:"Foundation/Shared/views/EntityView",component:t,tags:["autodocs"],argTypes:{...R([w,Z,_,z,P],t)}},n={args:{title:"MIZAR CDC",subtitle:"93000000000000128751",breadcrumbs:[{title:"Lorem",to:"/"},{title:"MIZAR CDC",disabled:!0}]},render:e=>({components:{FSEntityView:t},setup(){return{args:e}},template:`
+        <FSEntityView
+          v-bind="args"
+        >
+          <template
+            v-for="n in 80"
+            :key="n"
+          >
+            <p>I am a simple text</p>
+          </template>
+        </FSEntityView>
+      `})},o={args:{title:"MIZAR CDC",subtitle:"93000000000000128751",description:"Description d'un four Mizar sous Lorem Ipsum en maquettes.",imageCover:!0,icon:"mdi-chemical-weapon",iconBackgroundColors:["#FF0000","#0000FF"],iconBackgroundVariant:"gradient",iconBorder:!1,iconColor:s.Dark,breadcrumbs:[{title:"Lorem",to:"/"},{title:"MIZAR CDC",disabled:!0}]},render:e=>({components:{FSEntityView:t,FSButtonEdit:k,FSButtonRemove:E,FSLoader:x,FSCol:I},setup(){return{args:e}},template:`
         <FSEntityView v-bind="args">
           <template #toolbar>
             <FSButtonEdit />
@@ -16,7 +27,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
             </template>
           </FSCol>
         </FSEntityView>
-      `})},r={args:{title:"MIZAR CDC",subtitle:"93000000000000128751",description:null,imageId:null,imageCover:!0,icon:"mdi-chemical-weapon",color:s.Primary,iconBackgroundColors:void 0},render:(e,{argTypes:n})=>({components:{VDivider:k,FSEntityView:t},props:Object.keys(n),setup(){return{args:e}},template:`
+      `})},r={args:{title:"MIZAR CDC",subtitle:"93000000000000128751",description:null,imageId:null,imageCover:!0,icon:"mdi-chemical-weapon",color:s.Primary,iconBackgroundColors:void 0},render:e=>({components:{VDivider:h,FSEntityView:t},setup(){return{args:e}},template:`
     <div style="display: flex; flex-direction: column; gap: 10px;">
       <div style="display: flex; gap: 10px;">
         <FSEntityView
@@ -30,7 +41,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
           :iconBackgroundColors="args.iconBackgroundColors"
         />
       </div>
-    </div>`})},i={args:{title:"MIZAR CDC With a longer title",subtitle:null,description:null,imageId:null,imageCover:!0,icon:"mdi-chemical-weapon",color:s.Error,iconBackgroundColors:["#A0FFA0","#A0A0FF"],iconBackgroundVariant:"gradient"},render:(e,{argTypes:n})=>({components:{VDivider:k,FSEntityView:t},props:Object.keys(n),setup(){return{args:e}},template:`
+    </div>`})},i={args:{title:"MIZAR CDC With a longer title",subtitle:null,description:null,imageId:null,imageCover:!0,icon:"mdi-chemical-weapon",color:s.Error,iconBackgroundColors:["#A0FFA0","#A0A0FF"],iconBackgroundVariant:"gradient"},render:e=>({components:{VDivider:h,FSEntityView:t},setup(){return{args:e}},template:`
     <div style="display: flex; flex-direction: column; gap: 10px;">
       <div style="display: flex; gap: 10px;">
         <FSEntityView
@@ -45,7 +56,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
           :iconBackgroundVariant="args.iconBackgroundVariant"
         />
       </div>
-    </div>`})},a={args:{title:"Extension",subtitle:"subtitle extttttttttt",description:"Description d'une extension Lorem Ipsum en maquettes.",imageId:"1",imageCover:!0,icon:"mdi-chemical-weapon",color:s.Primary,iconBackgroundColors:void 0,breadcrumbs:[{title:"Lorem",to:"/"},{title:"MIZAR CDC",disabled:!0}]},render:(e,{argTypes:n})=>({components:{FSEntityView:t,FSButtonEdit:x,FSButtonRemove:f,FSLoader:B,FSCol:I,FSImage:$},props:Object.keys(n),setup(){return{args:e}},template:`
+    </div>`})},a={args:{title:"Extension",subtitle:"subtitle extttttttttt",description:"Description d'une extension Lorem Ipsum en maquettes.",imageId:"1",imageCover:!0,icon:"mdi-chemical-weapon",color:s.Primary,iconBackgroundColors:void 0,breadcrumbs:[{title:"Lorem",to:"/"},{title:"MIZAR CDC",disabled:!0}]},render:e=>({components:{FSEntityView:t,FSButtonEdit:k,FSButtonRemove:E,FSLoader:x,FSCol:I,FSImage:M},setup(){return{args:e}},template:`
         <FSEntityView v-bind="args">
           <template #toolbar>
             <FSButtonEdit />
@@ -74,7 +85,41 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
             </template>
           </FSCol>
         </FSEntityView>
-      `})};var p,l,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+      `})};var l,p,d;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    title: 'MIZAR CDC',
+    subtitle: '93000000000000128751',
+    breadcrumbs: [{
+      title: 'Lorem',
+      to: '/'
+    }, {
+      title: 'MIZAR CDC',
+      disabled: true
+    }]
+  },
+  render: args => ({
+    components: {
+      FSEntityView
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+        <FSEntityView
+          v-bind="args"
+        >
+          <template
+            v-for="n in 80"
+            :key="n"
+          >
+            <p>I am a simple text</p>
+          </template>
+        </FSEntityView>
+      \`
+  })
+}`,...(d=(p=n.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var c,u,g;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
   args: {
     title: 'MIZAR CDC',
     subtitle: '93000000000000128751',
@@ -94,9 +139,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       disabled: true
     }]
   },
-  render: (args, {
-    argTypes
-  }) => ({
+  render: args => ({
     components: {
       FSEntityView,
       FSButtonEdit,
@@ -104,7 +147,6 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       FSLoader,
       FSCol
     },
-    props: Object.keys(argTypes),
     setup() {
       return {
         args
@@ -130,7 +172,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
         </FSEntityView>
       \`
   })
-}`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var d,g,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var F,S,C;r.parameters={...r.parameters,docs:{...(F=r.parameters)==null?void 0:F.docs,source:{originalSource:`{
   args: {
     title: 'MIZAR CDC',
     subtitle: '93000000000000128751',
@@ -141,14 +183,11 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
     color: ColorEnum.Primary,
     iconBackgroundColors: undefined
   },
-  render: (args, {
-    argTypes
-  }) => ({
+  render: args => ({
     components: {
       VDivider,
       FSEntityView
     },
-    props: Object.keys(argTypes),
     setup() {
       return {
         args
@@ -170,7 +209,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       </div>
     </div>\`
   })
-}`,...(u=(g=r.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var F,C,S;i.parameters={...i.parameters,docs:{...(F=i.parameters)==null?void 0:F.docs,source:{originalSource:`{
+}`,...(C=(S=r.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var v,y,b;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
   args: {
     title: 'MIZAR CDC With a longer title',
     subtitle: null,
@@ -182,14 +221,11 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
     iconBackgroundColors: ['#A0FFA0', '#A0A0FF'],
     iconBackgroundVariant: 'gradient'
   },
-  render: (args, {
-    argTypes
-  }) => ({
+  render: args => ({
     components: {
       VDivider,
       FSEntityView
     },
-    props: Object.keys(argTypes),
     setup() {
       return {
         args
@@ -212,7 +248,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       </div>
     </div>\`
   })
-}`,...(S=(C=i.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var v,y,b;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+}`,...(b=(y=i.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var f,V,B;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
   args: {
     title: 'Extension',
     subtitle: 'subtitle extttttttttt',
@@ -230,9 +266,7 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       disabled: true
     }]
   },
-  render: (args, {
-    argTypes
-  }) => ({
+  render: args => ({
     components: {
       FSEntityView,
       FSButtonEdit,
@@ -241,7 +275,6 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
       FSCol,
       FSImage
     },
-    props: Object.keys(argTypes),
     setup() {
       return {
         args
@@ -278,4 +311,4 @@ import{d as h,z as D,B as L,S as A,m as R,G as M,H as _,C as j,N as z,O,P as Z}f
         </FSEntityView>
       \`
   })
-}`,...(b=(y=a.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};const en=["Default","Variation1","Variation2","ExtensionMock"];export{o as Default,a as ExtensionMock,r as Variation1,i as Variation2,en as __namedExportsOrder,Ye as default};
+}`,...(B=(V=a.parameters)==null?void 0:V.docs)==null?void 0:B.source}}};const pt=["Default","Variation0","Variation1","Variation2","ExtensionMock"];export{n as Default,a as ExtensionMock,o as Variation0,r as Variation1,i as Variation2,pt as __namedExportsOrder,lt as default};
