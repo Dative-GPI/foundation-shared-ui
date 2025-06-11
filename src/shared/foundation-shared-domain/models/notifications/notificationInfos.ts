eabdcfc7aa0058b0192d9d8993ee99cefabf458e
ddonfack <ddonfack@dative-gpi.com>
2025-06-11 15:13:20 +0200

    remove getdrawer

--- src/shared/foundation-shared-domain/models/notifications/notificationInfos.ts
+++ src/shared/foundation-shared-domain/models/notifications/notificationInfos.ts
@@ -1,7 +1,6 @@
 import { type MessageType } from "../../enums/messages";
 import { isoToEpoch } from "../../tools/datesTools";
-import { Scope } from "../../enums/applications";
-import { Criticity } from "../../enums/alerts";
+import type { Criticity } from "../../enums/alerts";
 import { NotificationAudience } from "./notificationAudience";
 
 export class NotificationInfos {
@@ -31,54 +30,6 @@ export class NotificationInfos {
     this.acknowledgingTimestamp = params.acknowledgingTimestamp ?
       isoToEpoch(params.acknowledgingTimestamp) : null;
   }
-
-  static getFromAudience = (
-    notifications: NotificationInfos[],
-    showPublic: boolean,
-    criticity: Criticity,
-    organisationId?: string | null,
-    userId?: string | null,
-    organisationTypeId?: string | null
-  ): NotificationInfos[] => notifications.filter((n: NotificationInfos) => {
-    if (n.acknowledged) {
-      return false;
-    }
-    if (![Criticity.None, n.criticity].includes(criticity)) {
-      return false;
-    }
-
-    return n.audiences.some(a => {
-      switch (a.targetScope) {
-        case Scope.OrganisationType:
-          return organisationTypeId && a.targetId === organisationTypeId;
-        case Scope.Organisation:
-        case Scope.UserOrganisation:
-          return organisationId && n.organisationId && n.organisationId === organisationId;
-        case Scope.User:
-          return userId && a.targetId === userId;
-        case Scope.Application:
-        case Scope.Public:
-          return showPublic;
-        default:
-          return false;
-      }
-    });
-  });
-
-  static getForDrawer = (
-    notifications: NotificationInfos[],
-    organisationId: string,
-    userId: string,
-    criticity: Criticity = Criticity.None,
-    organisationTypeId?: string | null
-  ): NotificationInfos[] => NotificationInfos.getFromAudience(notifications, true, criticity, organisationId, userId, organisationTypeId);
-
-  static getForCard = (
-    notifications: NotificationInfos[],
-    organisationId: string,
-    criticity: Criticity = Criticity.None,
-    organisationTypeId?: string | null
-  ): NotificationInfos[] => NotificationInfos.getFromAudience(notifications, false, criticity, organisationId, null, organisationTypeId);
 }
 
 export interface NotificationInfosDTO {
