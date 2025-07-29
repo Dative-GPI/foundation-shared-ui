290ae0ab2a28e6f895cbe9b420ca3e8d9f2abc25
ddonfack <ddonfack@dative-gpi.com>
2025-07-29 15:20:15 +0200

    remove token

--- src/shared/foundation-shared-domain/models/terminalApplications/terminalApplicationInfos.ts
+++ src/shared/foundation-shared-domain/models/terminalApplications/terminalApplicationInfos.ts
@@ -5,22 +5,17 @@ export class TerminalApplicationInfos {
   userId: string;
   label: string;
   icon: string;
-  key: string;
   location: string;
   lastConnection: number;
-  token: string;
 
   constructor(params: TerminalApplicationInfosDTO) {
     this.id = params.id;
     this.userId = params.userId;
     this.label = params.label;
     this.icon = params.icon;
-    this.key = params.key;
     this.location = params.location;
     this.lastConnection = isoToEpoch(params.lastConnection);
-    this.token = params.token;
   }
-
 }
 
 export interface TerminalApplicationInfosDTO {
@@ -28,12 +23,10 @@ export interface TerminalApplicationInfosDTO {
   userId: string;
   label: string;
   icon: string;
-  key: string;
   location: string;
   lastConnection: string;
-  token: string;
 }
 
 export interface TerminalApplicationFilters {
-  token?: string | null;
+  search?: string | null;
 }
