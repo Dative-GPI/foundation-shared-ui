bc53cae7ca6efdcffc08e3a34b5912bb12c43f18
ddonfack <ddonfack@dative-gpi.com>
2025-07-29 10:42:30 +0200

    adding FSCaroussel

---
+++ src/shared/foundation-shared-components/components/carousels/FSCarousel.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-carousel
+    v-bind="$attrs"
+  >
+    <slot/>
+  </v-carousel>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FSCarousel"
+});
+</script>
