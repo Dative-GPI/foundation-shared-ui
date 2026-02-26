6d083f50433a2ce0d3bf7a558446a626e8a39255
peyronth <peyronth@users.noreply.github.com>
2026-02-26 07:30:53 +0000

    deploy: 8bfcfdfce4573e79b5bbdb127f10622f4384bc50

---
+++ assets/BaseSubgroupingsList.stories-DM8uEyZb.js
@@ -0,0 +1,82 @@
+import r from"./FSBaseSubgroupingsList-CZGpNdCb.js";import{F as b}from"./FSDataTableUI-DtuEMzjg.js";import{F as p}from"./FSDataTable-DmblkUq1.js";import{F as S}from"./FSCard-DENSLTM_.js";import{a as V,b as F}from"./properties-Qw-O9fbT.js";import"./vue.esm-bundler-D5n7z5qz.js";import"./lodash-DyRFk5BS.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./FSEntityCountBadge-DY67PQVj.js";import"./FSColor-CsxJ8it1.js";import"./useColors-COEXNh00.js";import"./theme-BcqfMidt.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FSSpan-BN7wkmby.js";import"./useBreakpoints-CEzKC_o8.js";import"./useSlots-CoecqEnp.js";import"./FSRow-DIk8KFbG.js";import"./css-CxcvJboa.js";import"./badge-D9p4Oj7n.js";import"./FSSimpleTileUI-Csj1FPWH.js";import"./FSIconCard-BlsFbvOA.js";import"./FSIcon-DFkRD0af.js";import"./VIcon-C3cN4B51.js";import"./color-CUHqxR_J.js";import"./useRender-Dce1Ng4u.js";import"./icons-DbptYgqQ.js";import"./size-BBDCx9Cw.js";import"./tag-CJsP8ksx.js";import"./FSImage-EC-hlP5h.js";import"./FSImageUI-BLy7Ukeh.js";import"./FSLoader-uhE9mCg0.js";import"./dimensions-D_nx6Jbk.js";import"./elevation-62d6MvS5.js";import"./locale-n4v42L7l.js";import"./proxiedModel-Cv3qT1cZ.js";import"./VImg-CFhtVVj8.js";import"./rounded-BSNeeq4o.js";import"./transition-gfLL8RoD.js";import"./index-u79WnTSR.js";import"./useImages-tmSeA2NQ.js";import"./base-CmdGny12.js";import"./composableFactory-zc0ZsFxC.js";import"./serviceFactory-CyDQptGk.js";import"./uuid-DTaye2KM.js";import"./useAppAuthToken-mdN0gbKX.js";import"./FSTile-BiFTCuMc.js";import"./FSCheckbox-DMg4fEo0.js";import"./FSCol-D_lbFmZy.js";import"./useRules-R3Ldkrx4.js";import"./VCheckboxBtn-ZIdy5nkM.js";import"./VSelectionControl-Cd6FgV1q.js";import"./density-BTT1solu.js";import"./index-C2v-jVLO.js";import"./VLabel-DPXmEVvJ.js";import"./VInput-CbNwc64i.js";import"./index-Bo6vw90T.js";import"./FSText-015IEZHy.js";import"./FSChip-BmAEKlmX.js";import"./useSubgroupings-C76r4X5S.js";import"./subgroupingInfos-CBtJpNmo.js";import"./base-YMsHX0Is.js";import"./useAppOrganisationId-CFu8M88R.js";import"./vue-router-BrikmAnQ.js";import"./FSSearchField-Cc3wHhUa.js";import"./FSTextField-DVC6JxU9.js";import"./FSBaseField-C_kMSpMl.js";import"./FSButton-M9GMz66l.js";import"./FSRouterLink-D3C4jXZ9.js";import"./VProgressCircular-BsmUxqlD.js";import"./resizeObserver-CpF7iUII.js";import"./VField-BWZy72Xk.js";import"./loader-CAuY8UYG.js";import"./VProgressLinear-CI-Q4csR.js";import"./anchor-tlySGI4g.js";import"./VDefaultsProvider-CPXI0q_G.js";import"./easing-DY7PVvcf.js";import"./forwardRefs-C-GTDzx5.js";import"./useTranslations-CFCS6HA-.js";import"./FSSelectField-Q_toR6Uv.js";import"./FSDialogMenu-B52P8q-X.js";import"./VDialog-C5WNai-3.js";import"./VOverlay-BApTxsTF.js";import"./display-DvHDH61C.js";import"./lazy-Bqido2cx.js";import"./router-R_WRJyUs.js";import"./scopeId-DI6aT8IG.js";import"./dialog-transition-CBLjbvr6.js";import"./FSSlideGroup-BnnHyPAm.js";import"./FSButtonNextIcon-BxxPSm84.js";import"./VSlideGroup-BJAU1y7G.js";import"./goto-nADK4Ssb.js";import"./group-Cx5pids1.js";import"./VSlideGroupItem-B1P3g7mq.js";import"./FSToggleSet-BO0lhIHf.js";import"./FSWrapGroup-ES-pUhBN.js";import"./FSFadeOut-YXyyFCAB.js";import"./FSRadio-CKU_VsjQ.js";import"./VSelect-Dn3JzDeK.js";import"./VList-C0qufsOv.js";import"./ssrBoot-C79LbZhX.js";import"./border-5JTJ2U38.js";import"./variant-CRuOwF35.js";import"./VDivider-IiSngj1L.js";import"./VMenu-DOme5rNf.js";import"./FSDivider-B5u6_Nl0.js";import"./FSMenu-mE5omh8N.js";import"./FSOptionGroup-BgGfz_tY.js";import"./filter-C1K_d8Vd.js";import"./VBtn-DtRd7_Uc.js";import"./position-DfaaINa4.js";import"./filter-CjCsq5wT.js";const fo={title:"Foundation/Core/Lists/Base Lists/BaseSubgroupingsList",component:r,subcomponents:{FSDataTable:p},tags:["autodocs"],argTypes:{...F([p,b,S],r),...V(r)}},t={render:o=>({components:{FSBaseSubgroupingsList:r},setup(){return{args:o}},template:`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    `}),args:{modelValue:[],tableCode:"subgroupings1"}},e={render:o=>({components:{FSBaseSubgroupingsList:r},setup(){return{args:o}},template:`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    `}),args:{modelValue:[],tableCode:"subgroupings1",selectable:!0}},m={render:o=>({components:{FSBaseSubgroupingsList:r},setup(){return{args:o}},template:`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    `}),args:{modelValue:[],tableCode:"subgroupings1",selectable:!0,singleSelect:!0}};var i,s,n;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseSubgroupingsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    \`
+  }),
+  args: {
+    modelValue: [],
+    tableCode: 'subgroupings1'
+  }
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var a,u,l;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseSubgroupingsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    \`
+  }),
+  args: {
+    modelValue: [],
+    tableCode: 'subgroupings1',
+    selectable: true
+  }
+}`,...(l=(u=e.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var g,d,c;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      FSBaseSubgroupingsList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <FSBaseSubgroupingsList
+        v-model:modelValue="args.modelValue"
+        v-bind="args"
+      />
+    \`
+  }),
+  args: {
+    modelValue: [],
+    tableCode: 'subgroupings1',
+    selectable: true,
+    singleSelect: true
+  }
+}`,...(c=(d=m.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const Do=["Default","Selectable","SingleSelect"];export{t as Default,e as Selectable,m as SingleSelect,Do as __namedExportsOrder,fo as default};
